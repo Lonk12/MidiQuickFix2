f8109ee322cbdf83b93733fe63ec8dc79348b80d
jostle <>
2004-12-30 03:37:12 +0000

    Get names from our properties file if the synth can not be found.

--- MidiQuickFix/src/com/lemckes/MidiQuickFix/InstrumentNames.java
+++ MidiQuickFix/src/com/lemckes/MidiQuickFix/InstrumentNames.java
@@ -1,33 +1,40 @@
 package com.lemckes.MidiQuickFix;
 
-import java.util.*;
+import java.util.ResourceBundle;
+
 
 /** Methods associated with MIDI instrument (patch) names. */
 class InstrumentNames {
-    /** The names of the instruments (patches) are retrieved from a resource file. */
-    static ResourceBundle mInstrumentsBundle;
+    
     static String[] mNameArray;
     
     /** Create an InstrumentNames object. */
     static {
-        mInstrumentsBundle = ResourceBundle.getBundle("com/lemckes/MidiQuickFix/resources/GM1Instruments");
-        int numInsts = Integer.parseInt(mInstrumentsBundle.getString("count"));
-        
-        javax.sound.midi.Synthesizer synth = null;
-        javax.sound.midi.Instrument[] instrument = null;
         try {
-            synth = javax.sound.midi.MidiSystem.getSynthesizer(); //
+            javax.sound.midi.Synthesizer synth = null;
+            javax.sound.midi.Instrument[] instruments = null;
+            synth = javax.sound.midi.MidiSystem.getSynthesizer();
+            instruments = synth.getAvailableInstruments();
+            
+            mNameArray = new String[instruments.length];
+            for (int j = 0; j < instruments.length; ++j) {
+                System.out.println(j + "   " + instruments[j].getName());
+                mNameArray[j] = instruments[j].getName();
+            }
         } catch (javax.sound.midi.MidiUnavailableException e) {
-        }
-        instrument = synth.getAvailableInstruments(); //
-        //             synth.getLoadedInstruments();
-        
-        // mNameArray = new String[numInsts];
-        mNameArray = new String[instrument.length];
-        for (int j = 0; j < instrument.length; ++j) {
-            // mNameArray[j] = mInstrumentsBundle.getString(new Integer(j + 1).toString());
-            System.out.println(j + "   " + instrument[j].getName());
-            mNameArray[j] = instrument[j].getName();
+            System.out.println("Failed to getSynthesizer() : " + e.getMessage());
+            System.out.println("  --  Using resource bundle for instrument names");
+            
+            ResourceBundle mInstrumentsBundle =
+                    ResourceBundle.getBundle("com/lemckes/MidiQuickFix/resources/GM1Instruments");
+            int numInsts =
+                    Integer.parseInt(mInstrumentsBundle.getString("count"));
+            
+            mNameArray = new String[numInsts];
+            for (int j = 0; j < numInsts; ++j) {
+                mNameArray[j] = mInstrumentsBundle.getString(new Integer(j + 1).toString());
+                System.out.println(j + "   " + mNameArray[j]);
+            }
         }
     }
     
