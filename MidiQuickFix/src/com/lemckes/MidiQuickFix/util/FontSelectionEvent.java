d588569ff8e82ce0b0471249013722a25b5c9b1c
jostle <>
2006-07-10 08:24:23 +0000

    Event classes for the FontSelector

---
+++ MidiQuickFix/src/com/lemckes/MidiQuickFix/util/FontSelectionEvent.java
@@ -0,0 +1,52 @@
+/**************************************************************
+ *
+ *   MidiQuickFix - A Simple Midi file editor and player
+ *
+ *   Copyright (C) 2004-2005 John Lemcke
+ *   jostle@users.sourceforge.net
+ *
+ *   This program is free software; you can redistribute it
+ *   and/or modify it under the terms of the Artistic License
+ *   as published by Larry Wall, either version 2.0,
+ *   or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the Artistic License for more details.
+ *
+ *   You should have received a copy of the Artistic License with this Kit,
+ *   in the file named "Artistic.clarified".
+ *   If not, I'll be glad to provide one.
+ *
+ **************************************************************/
+
+package com.lemckes.MidiQuickFix.util;
+
+import java.awt.Font;
+
+/**
+ * The event that is passed to FontSelectionListener implementors.
+ * @see FontSelectionListener
+ * @version $Id$
+ */
+public class FontSelectionEvent {
+    /**
+     * Create a FontSelectionEvent.
+     * @param newFont The font that has been selected.
+     */
+    public FontSelectionEvent(Font newFont) {
+        mFont = newFont;
+    }
+    
+    /**
+     * Get the selected font
+     * @return the selected font
+     */
+    public Font getSelectedFont() {
+        return mFont;
+    }
+    
+    private Font mFont;
+}
+
