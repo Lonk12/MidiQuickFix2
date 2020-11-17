7af1c10622d863b7882292f6ac1c7591dfe35702
John Lemcke <jostle@jostlesoft.com>
2020-11-17 18:52:39 +1100

    Change from GeneralPath to Path2D.Float. Scale the border

--- MidiQuickFix/src/com/lemckes/MidiQuickFix/util/DrawnIcon.java
+++ MidiQuickFix/src/com/lemckes/MidiQuickFix/util/DrawnIcon.java
@@ -1,4 +1,4 @@
-/**************************************************************
+/** ************************************************************
  *
  *   MidiQuickFix - A Simple Midi file editor and player
  *
@@ -19,7 +19,7 @@
  *   in the file named "Artistic.clarified".
  *   If not, I'll be glad to provide one.
  *
- **************************************************************/
+ ************************************************************* */
 package com.lemckes.MidiQuickFix.util;
 
 import java.awt.BasicStroke;
@@ -29,19 +29,20 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.GeneralPath;
+import java.awt.geom.Path2D;
 import java.awt.geom.Rectangle2D;
 import javax.swing.AbstractButton;
 import javax.swing.Icon;
 
 /**
- * An Icon that draws itself using a GeneralPath.
+ * An Icon that draws itself using a Path2D.Float.
  */
-public class DrawnIcon implements Icon
+public class DrawnIcon
+    implements Icon
 {
 
     private AbstractButton mParent;
-    private GeneralPath mPath;
+    private Path2D.Float mPath;
     private Color mFillColour = Color.GRAY;
     private Color mBorderColour = Color.BLACK;
     private Color mActiveBorderColour = Color.WHITE;
@@ -51,19 +52,21 @@ public class DrawnIcon implements Icon
 
     /**
      * Create a new instance of DrawnIcon with the default shape
+     *
      * @param parent the AbstractButton on which this icon is drawn
      */
     public DrawnIcon(AbstractButton parent) {
         this(parent,
-            new GeneralPath(new Rectangle2D.Double(0.3, 0.3, 0.4, 0.4)));
+            new Path2D.Float(new Rectangle2D.Float(0.3f, 0.3f, 0.4f, 0.4f)));
     }
 
     /**
      * Create a new instance of DrawnIcon with the given shape
+     *
      * @param parent the AbstractButton on which this icon is drawn
-     * @param shape the GeneralPath used to draw this icon
+     * @param shape the Path2D.Float used to draw this icon
      */
-    public DrawnIcon(AbstractButton parent, GeneralPath shape) {
+    public DrawnIcon(AbstractButton parent, Path2D.Float shape) {
         this.mParent = parent;
         this.mPath = shape;
         mFilled = true;
@@ -72,30 +75,30 @@ public class DrawnIcon implements Icon
 
     @Override
     public int getIconHeight() {
-        return 12;
+        return 16;
     }
 
     @Override
     public int getIconWidth() {
-        return 12;
+        return 16;
     }
 
     @Override
     public void paintIcon(Component component, Graphics g, int x, int y) {
         Graphics2D g2 = (Graphics2D)g;
         AffineTransform savedAT = g2.getTransform();
-        double xScale = component.getWidth();
-        double yScale = component.getHeight();
+        float xScale = component.getWidth();
+        float yScale = component.getHeight();
 
         // Maintain a square aspect ratio for the icon.
-        double scale = Math.min(xScale, yScale);
+        float scale = Math.min(xScale, yScale);
         // Set the scale to give the smallest dimension a size of 1.0
-        AffineTransform at =
-            AffineTransform.getScaleInstance(scale, scale);
+        AffineTransform at
+            = AffineTransform.getScaleInstance(scale, scale);
 
         // Translate either X or Y to centre the icon in the largest dimension
-        double xTrans = xScale > yScale ? (xScale - yScale) / (2 * yScale) : 0;
-        double yTrans = yScale > xScale ? (yScale - xScale) / (2 * xScale) : 0;
+        float xTrans = xScale > yScale ? (xScale - yScale) / (2 * yScale) : 0;
+        float yTrans = yScale > xScale ? (yScale - xScale) / (2 * xScale) : 0;
 
         at.translate(xTrans, yTrans);
 
@@ -108,7 +111,7 @@ public class DrawnIcon implements Icon
         }
 
         if (mBordered) {
-            drawBorder(g2);
+            drawBorder(g2, scale);
         }
 
         g2.setTransform(savedAT);
@@ -121,7 +124,7 @@ public class DrawnIcon implements Icon
         g2.fill(mPath);
     }
 
-    private void drawBorder(Graphics2D g2) {
+    private void drawBorder(Graphics2D g2, float scale) {
         boolean pressed = mParent.getModel().isArmed();
         boolean selected = mParent.isSelected();
 
@@ -130,15 +133,21 @@ public class DrawnIcon implements Icon
         } else {
             g2.setColor(mBorderColour);
         }
-        g2.setStroke(new BasicStroke(0.02f));
+        float strokeWidth
+            = scale < 16 ? 0.5f
+                : scale < 64 ? 0.5f + ((scale - 16) / 48) * 0.5f
+                    : scale < 256 ? 1.0f + ((scale - 64) / 192) * 1.0f
+                        : scale < 1024 ? 2.0f + ((scale - 256) / 768) * 2.0f
+                            : 4;
+        g2.setStroke(new BasicStroke(strokeWidth / scale));
         g2.draw(mPath);
     }
 
-    public GeneralPath getPath() {
+    public Path2D.Float getPath() {
         return mPath;
     }
 
-    public void setPath(GeneralPath path) {
+    public void setPath(Path2D.Float path) {
         this.mPath = path;
     }
 
@@ -158,15 +167,11 @@ public class DrawnIcon implements Icon
         this.mBorderColour = borderColour;
 
         // Calculate a contrasting colour for the activeBorderColour
-        float hue =
-            (Color.RGBtoHSB(borderColour.getRed(),
-            borderColour.getGreen(), borderColour.getBlue(), null))[0];
-        float sat =
-            (Color.RGBtoHSB(borderColour.getRed(),
-            borderColour.getGreen(), borderColour.getBlue(), null))[1];
-        float bri =
-            (Color.RGBtoHSB(borderColour.getRed(),
-            borderColour.getGreen(), borderColour.getBlue(), null))[2];
+        float[] hsbVals = Color.RGBtoHSB(borderColour.getRed(),
+            borderColour.getGreen(), borderColour.getBlue(), null);
+        float hue = hsbVals[0];
+        float sat = hsbVals[1];
+        float bri = hsbVals[2];
         if (sat < 0.2) {
             // This is a grey or close to it so adjust the brightness
             if (bri > 0.4 && bri < 0.6) {
@@ -176,12 +181,12 @@ public class DrawnIcon implements Icon
                 // otherwise invert the brightness
                 bri = 1.0f - bri;
             }
-            this.mActiveBorderColour =
-                Color.getHSBColor(hue, sat, bri);
+            this.mActiveBorderColour
+                = Color.getHSBColor(hue, sat, bri);
         } else {
             // This is not grey so adjust the hue.
-            this.mActiveBorderColour =
-                Color.getHSBColor(hue + 0.5f, sat, bri);
+            this.mActiveBorderColour
+                = Color.getHSBColor(hue + 0.5f, sat, bri);
         }
     }
 
